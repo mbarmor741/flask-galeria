fd8a18f90a17f348e1a46c4092f2ed76c0e9e920
mbarmor741 <120595085+mbarmor741@users.noreply.github.com>
2023-05-15 18:17:26 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -2,20 +2,33 @@
 # Nota: en ese tutorial se ve como hacerlo
 # - con Heroku CLI
 # - sin usar GitHub
-#   Aunque luego al final lo manda a GitHub
+# Aunque luego al final lo manda a GitHub
+
 
 from flask import Flask, render_template
 from datetime import datetime
 import locale
 
+
+
+
 app = Flask(__name__)
 locale.setlocale(locale.LC_TIME, '')
 
+
+
+
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %H:%M")
-    # Render HTML with variable
-    return render_template("index.html", the_time=the_time, tema="dogs")
+
+
+the_time = datetime.now().strftime("%A, %d %b %Y %H:%M")
+
+
+# Render HTML with variable
+return render_template("index.html", the_time=the_time, tema="dogs")
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+app.run(debug=True, use_reloader=True)
